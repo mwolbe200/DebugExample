e2171ab9e49cc703794bc02874d316ff2f994c4c
EAGCU <137394555+EAGCU@users.noreply.github.com>
2023-09-19 15:36:20 +0100

    update

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,40 +1,114 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.1.1
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
 
   ==============================================================================
 */
 
-#include "PluginProcessor.h"
+//[Headers] You can add your own extra header files here...
+//[/Headers]
+
 #include "PluginEditor.h"
 
-//==============================================================================
-StereoPannerAudioProcessorEditor::StereoPannerAudioProcessorEditor (StereoPannerAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
-{
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
-}
 
-StereoPannerAudioProcessorEditor::~StereoPannerAudioProcessorEditor()
-{
-}
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
 
 //==============================================================================
-void StereoPannerAudioProcessorEditor::paint (juce::Graphics& g)
+DebugExampleAudioProcessorEditor::DebugExampleAudioProcessorEditor (DebugExampleAudioProcessor& p)
+    : AudioProcessorEditor(p), processor(p)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    //[Constructor_pre] You can add your own custom stuff here..
+    //[/Constructor_pre]
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+
+    //[UserPreSize]
+    //[/UserPreSize]
+
+    setSize (400, 400);
+
+
+    //[Constructor] You can add your own custom stuff here..
+    //[/Constructor]
 }
 
-void StereoPannerAudioProcessorEditor::resized()
+DebugExampleAudioProcessorEditor::~DebugExampleAudioProcessorEditor()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
 }
+
+//==============================================================================
+void DebugExampleAudioProcessorEditor::paint (Graphics& g)
+{
+    //[UserPrePaint] Add your own custom painting code here..
+    //[/UserPrePaint]
+
+    g.fillAll (Colours::black);
+
+    //[UserPaint] Add your own custom painting code here..
+    //[/UserPaint]
+}
+
+void DebugExampleAudioProcessorEditor::resized()
+{
+    //[UserPreResize] Add your own custom resize code here..
+    //[/UserPreResize]
+
+    //[UserResized] Add your own custom resize handling here..
+    //[/UserResized]
+}
+
+
+
+//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
+
+//[/MiscUserCode]
+
+
+//==============================================================================
+#if 0
+/*  -- Projucer information section --
+
+    This is where the Projucer stores the metadata that describe this GUI layout, so
+    make changes in here at your peril!
+
+BEGIN_JUCER_METADATA
+
+<JUCER_COMPONENT documentType="Component" className="DebugExampleAudioProcessorEditor"
+                 componentName="" parentClasses="public AudioProcessorEditor, public Timer"
+                 constructorParams="DebugExampleAudioProcessor&amp; p" variableInitialisers="AudioProcessorEditor(p), processor(p)&#10;"
+                 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
+                 fixedSize="0" initialWidth="400" initialHeight="400">
+  <BACKGROUND backgroundColour="ff000000"/>
+  <SLIDER name="new slider" id="ceaa68e8ca3b691c" memberName="sliderPanPosition"
+          virtualName="" explicitFocusOrder="0" pos="24 32 176 120" min="-1"
+          max="1" int="0" style="Rotary" textBoxPos="TextBoxBelow" textBoxEditable="1"
+          textBoxWidth="80" textBoxHeight="20" skewFactor="1" needsCallback="1"/>
+  <TOGGLEBUTTON name="new toggle button" id="b88aa77a47a911c7" memberName="buttonPanningAlgorithm"
+                virtualName="" explicitFocusOrder="0" pos="72 184 150 24" buttonText="Linear / Constant"
+                connectedEdges="0" needsCallback="1" radioGroupId="0" state="0"/>
+</JUCER_COMPONENT>
+
+END_JUCER_METADATA
+*/
+#endif
+
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,37 +1,64 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.1.1
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
 
   ==============================================================================
 */
 
 #pragma once
 
-#include <JuceHeader.h>
+//[Headers]     -- You can add your own extra header files here --
+#include "JuceHeader.h"
 #include "PluginProcessor.h"
+//[/Headers]
+
+
 
 //==============================================================================
 /**
+                                                                    //[Comments]
+    An auto-generated component, created by the Introjucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
 */
-class StereoPannerAudioProcessorEditor  : public juce::AudioProcessorEditor
+class DebugExampleAudioProcessorEditor  : public AudioProcessorEditor
 {
 public:
-    StereoPannerAudioProcessorEditor (StereoPannerAudioProcessor&);
-    ~StereoPannerAudioProcessorEditor() override;
+    //==============================================================================
+    DebugExampleAudioProcessorEditor (DebugExampleAudioProcessor& p);
+    ~DebugExampleAudioProcessorEditor();
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
     void resized() override;
 
+
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    StereoPannerAudioProcessor& audioProcessor;
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    DebugExampleAudioProcessor& processor;
+    //[/UserVariables]
 
+    //==============================================================================
 
-
-
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (StereoPannerAudioProcessorEditor)
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DebugExampleAudioProcessorEditor)
 };
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
